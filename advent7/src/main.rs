92924691b78ad56d1800966573cd3a9eea8a51b0
arborelia <gh@arborelia.net>
2022-12-07 02:28:15 -0500

    day 7 done

--- advent7/src/main.rs
+++ advent7/src/main.rs
@@ -1,6 +1,7 @@
 // An example to build from each day
 use std::fs;
 use std::error::Error;
+use std::collections::HashMap;
 use parse_display::{Display, FromStr};
 
 pub const TEST_INPUT: &str = "$ cd /
@@ -29,13 +30,6 @@ $ ls
 
 
 #[derive(Display, FromStr, PartialEq, Debug, Clone)]
-#[display("{size} {name}")]  
-struct File {
-    name: String,
-    size: i64
-}
-
-#[derive(Display, FromStr, PartialEq, Debug, Clone)]
 enum ElfShellLine {
     #[display("$ ls")]
     ListFiles,
@@ -43,27 +37,41 @@ enum ElfShellLine {
     ChangeDir { destination: String },
     #[display("dir {name}")]
     DirEntry { name: String },
-    #[display("{file}")]  
-    FileEntry { file: File },
+    #[display("{size} {name}")]
+    FileEntry { name: String, size: i64 },
 }
 
 #[derive(Debug)]
 enum FilesystemEntry {
-    DirEntry { contents: Vec<FilesystemEntry> },
-    FileEntry { file: File },
+    Dir { contents: HashMap<String, FilesystemEntry> },
+    File { size: i64 },
 }
 
 impl FilesystemEntry {
-    fn put_in_path(&mut self, path: &[String], file: &File) {
+    fn put_in_path(&mut self, path: &[String], filename: &str, size: i64) {
         match self {
-            FilesystemEntry::FileEntry { file: _ } => {
+            FilesystemEntry::File { size: _ } => {
                 panic!("did we cd into a file?");
             },
-            FilesystemEntry::DirEntry { contents } => {
+            FilesystemEntry::Dir { contents } => {
                 if path.len() == 0 {
-                    contents.push(FilesystemEntry::FileEntry { file: file.clone() });
+                    contents.insert(filename.to_owned(), FilesystemEntry::File { size });
                 } else {
-                    self.put_in_path(&path[1..], file)
+                    let path_component: &str = &path[0];
+                    if !(contents.contains_key(path_component)) {
+                        contents.insert(
+                            path_component.to_owned(), 
+                            FilesystemEntry::Dir { contents: HashMap::new() }
+                        );
+                    }
+                    match contents.get_mut(path_component) {
+                        Some(entry) => {
+                            entry.put_in_path(&path[1..], filename, size);
+                        },
+                        None => {
+                            panic!("I swear we just added this directory");
+                        }
+                    }
                 }
             }
         }
@@ -71,34 +79,80 @@ impl FilesystemEntry {
 
     fn size(&self) -> i64 {
         match self {
-            FilesystemEntry::FileEntry { file } => file.size,
-            FilesystemEntry::DirEntry { contents } => {
-                contents.iter().map(|entry| entry.size()).sum()
-                /* let mut total: i64 = 0;
-                for entry in contents {
-                    total += entry.size();
-                }
-                total */
+            FilesystemEntry::File { size } => *size,
+            FilesystemEntry::Dir { contents } => {
+                contents.values().map(|entry| entry.size()).sum()
             }
         }
     }
+
+    fn depth_first_dirs(&self) -> Vec<&FilesystemEntry> {
+        let mut dirs = Vec::new();
+        match self {
+            FilesystemEntry::File { size: _ } => dirs,
+            FilesystemEntry::Dir { contents } => {
+                dirs.push(&self);
+                for entry in contents.values() {
+                    dirs.extend(entry.depth_first_dirs().iter());
+                }
+                dirs
+            }
+        }
+    }
+
+    fn size_of_small_dirs(&self, cutoff: i64) -> i64 {
+        match self {
+            FilesystemEntry::File { size: _ } => 0,
+            FilesystemEntry::Dir { contents: _ } => {
+                let mut total: i64 = 0;
+                for dir in self.depth_first_dirs() {
+                    if dir.size() <= cutoff {
+                        total += dir.size();
+                    }
+                }
+                total
+            }
+        }
+    }
+
+    fn find_dir_to_delete(&self, min_size: i64) -> &FilesystemEntry {
+        let mut dir_to_delete: Option<&FilesystemEntry> = None;
+        let mut smallest_size: i64 = i64::MAX;
+        for dir in self.depth_first_dirs() {
+            let size = dir.size();
+            if size >= min_size && size < smallest_size {
+                dir_to_delete = Some(dir);
+                smallest_size = size;
+            }
+        }
+        dir_to_delete.unwrap()
+    }
+
+    fn size_of_dir_to_delete(&self, space_needed: i64) -> i64 {
+        let min_size: i64 = self.size() - space_needed;
+        self.find_dir_to_delete(min_size).size()
+    }
 }
+
+
 
 fn read_dir_tree(input: &str) -> Result<FilesystemEntry, Box<dyn Error>> {
     let mut path: Vec<String> = Vec::new();
-    let mut filesystem = FilesystemEntry::DirEntry { contents: Vec::new() };
+    let mut filesystem = FilesystemEntry::Dir { contents: HashMap::new() };
     for line in input.lines() {
         let shell: ElfShellLine = line.parse()?;
         match shell {
             ElfShellLine::ChangeDir { destination } => {
                 if destination == ".." {
                     path.pop();
+                } else if destination == "/" {
+                    path.drain(..);
                 } else {
                     path.push(destination.clone());
                 }
             },
-            ElfShellLine::FileEntry { file } => {
-                filesystem.put_in_path(&path, &file);
+            ElfShellLine::FileEntry { name, size } => {
+                filesystem.put_in_path(&path, &name, size);
             },
             ElfShellLine::DirEntry { name: _ } => {},
             ElfShellLine::ListFiles => {}
@@ -110,6 +164,8 @@ fn read_dir_tree(input: &str) -> Result<FilesystemEntry, Box<dyn Error>> {
 fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string("input.txt")?;
     let filesystem = read_dir_tree(&input)?;
+    println!("sum of small directories: {}", filesystem.size_of_small_dirs(100_000));
+    println!("size of dir to delete: {}", filesystem.size_of_dir_to_delete(40_000_000));
     Ok(())
 }
 
@@ -124,11 +180,23 @@ mod tests {
         let sh: ElfShellLine = "$ cd here".parse().unwrap();
         assert_eq!(sh, ElfShellLine::ChangeDir { destination: "here".to_owned() });
         let sh: ElfShellLine = "123 filename".parse().unwrap();
-        assert_eq!(sh, ElfShellLine::FileEntry { file: File { name: "filename".to_owned(), size: 123 }});
+        assert_eq!(sh, ElfShellLine::FileEntry { name: "filename".to_owned(), size: 123 });
     }
 
     #[test]
     fn test_full_parse() {
-        read_dir_tree(TEST_INPUT).unwrap();
+        let filesystem = read_dir_tree(TEST_INPUT).unwrap();
+    }
+
+    #[test]
+    fn test_example() {
+        let filesystem = read_dir_tree(TEST_INPUT).unwrap();
+        assert_eq!(filesystem.size_of_small_dirs(100_000), 95437);
+    }
+
+    #[test]
+    fn test_example_2() {
+        let filesystem = read_dir_tree(TEST_INPUT).unwrap();
+        assert_eq!(filesystem.size_of_dir_to_delete(40_000_000), 24933642);
     }
 }
