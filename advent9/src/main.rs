1154c0342b9981aa74ce36a75fb1973637c24f10
arborelia <gh@arborelia.net>
2022-12-09 01:19:32 -0500

    day 9 part 2 done

--- advent9/src/main.rs
+++ advent9/src/main.rs
@@ -13,6 +13,15 @@ D 1
 L 5
 R 2";
 
+pub const BIGGER_TEST_INPUT: &str = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
+
 // the rope has a head and tail, and is on an integer grid
 // tail is always within 1 space of the head, orthogonally or diagonally
 // it gets pulled toward the head, orthogonally or diagonally
@@ -40,49 +49,49 @@ struct Position {
     y: i64,
 }
 
-#[derive(PartialEq, Debug)]
-struct Rope {
-    head: Position,
-    tail: Position,
+fn make_rope(length: usize) -> Vec<Position> {
+    vec![Position { x: 0, y: 0 }; length]
 }
 
 /// Move the rope and return the new position of the
-fn move_rope(rope: &mut Rope, mov: &RopeMove, visited: &mut HashSet<Position>) {
+fn move_rope(rope: &mut Vec<Position>, mov: &RopeMove, visited: &mut HashSet<Position>) {
     for _ in 0..mov.num {
         match mov.dir {
-            Direction::U => rope.head.y -= 1,
-            Direction::D => rope.head.y += 1,
-            Direction::L => rope.head.x -= 1,
-            Direction::R => rope.head.x += 1,
+            Direction::U => rope[0].y -= 1,
+            Direction::D => rope[0].y += 1,
+            Direction::L => rope[0].x -= 1,
+            Direction::R => rope[0].x += 1,
         }
-        let dist = max(
-            (rope.head.x - rope.tail.x).abs(),
-            (rope.head.y - rope.tail.y).abs(),
-        );
-        if dist >= 2 {
-            if rope.head.x > rope.tail.x {
-                rope.tail.x += 1;
-            } else if rope.head.x < rope.tail.x {
-                rope.tail.x -= 1;
-            }
-            if rope.head.y > rope.tail.y {
-                rope.tail.y += 1;
-            } else if rope.head.y < rope.tail.y {
-                rope.tail.y -= 1;
+        for idx in 0..(rope.len() - 1) {
+            let head = rope[idx];
+            let link = &mut rope[idx + 1];
+            let dist = max(
+                (head.x - link.x).abs(),
+                (head.y - link.y).abs(),
+            );
+            if dist >= 2 {
+                if head.x > link.x {
+                    link.x += 1;
+                } else if head.x < link.x {
+                    link.x -= 1;
+                }
+                if head.y > link.y {
+                    link.y += 1;
+                } else if head.y < link.y {
+                    link.y -= 1;
+                }
             }
         }
-        visited.insert(rope.tail);
+        let tail = rope[rope.len() - 1];
+        visited.insert(tail);
     }
 }
 
-fn count_tail_positions(input: &str) -> i64 {
+fn count_tail_positions(input: &str, rope_length: usize) -> i64 {
     let mut visited: HashSet<Position> = HashSet::new();
     visited.insert(Position { x: 0, y: 0 });
 
-    let mut rope = Rope {
-        head: Position { x: 0, y: 0 },
-        tail: Position { x: 0, y: 0 },
-    };
+    let mut rope = make_rope(rope_length);
 
     for line in input.split("\n") {
         let mov: RopeMove = line.parse().unwrap();
@@ -93,7 +102,8 @@ fn count_tail_positions(input: &str) -> i64 {
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-    println!("tail positions visited: {}", count_tail_positions(&input));
+    println!("tail positions visited with length 2: {}", count_tail_positions(&input, 2));
+    println!("tail positions visited with length 10: {}", count_tail_positions(&input, 10));
 }
 
 #[cfg(test)]
@@ -113,6 +123,12 @@ mod tests {
 
     #[test]
     fn test_example() {
-        assert_eq!(count_tail_positions(TEST_INPUT), 13);
+        assert_eq!(count_tail_positions(TEST_INPUT, 2), 13);
+        assert_eq!(count_tail_positions(TEST_INPUT, 10), 1);
+    }
+
+    #[test]
+    fn test_bigger_example() {
+        assert_eq!(count_tail_positions(BIGGER_TEST_INPUT, 10), 36);
     }
 }
